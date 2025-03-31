9f8a81ba87687940a57327a43173af0c1db09b2b
Nikolay <nikolayzhidkov@icloud.com>
2025-03-31 22:21:33 +0700

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Calendar from './components/Calendar';
 import './App.css';
-import { Event } from './types/event'; 
+import { Event } from './types/event'; // Правильный путь импорта
 
 const App: React.FC = () => {
     const [events, setEvents] = useState<Record<string, Event[]>>({});
+    const userId = 749991690;
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjc0OTk5MTY5MCwiaWF0IjoxNzQzNDQwNTY4LCJleHAiOjE3NzQ5NzY1Njh9.wtf6a9w2q0UqQUmDXuxiehN_B4RXD93TkdUlFwC6skY";
 
     useEffect(() => {
         const fetchData = async () => {
@@ -13,8 +15,9 @@ const App: React.FC = () => {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`, // Добавили заголовок Authorization
                     },
-                    body: JSON.stringify({ t_user_id: 749991690 }),
+                    body: JSON.stringify({ t_user_id: userId }), // Тело запроса нужно
                 });
 
                 if (!response.ok) {
